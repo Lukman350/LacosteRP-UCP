2c2d0d9c642dfb2c39567998f9f55e29cbeac476
Lukman <61648308+Lukman350@users.noreply.github.com>
2021-03-13 09:41:55 +0700

    Update login.php

--- login.php
+++ login.php
@@ -25,30 +25,31 @@ if (isset($_SESSION["user"]))
   </script>
 </head>
 <body>
-  <div class="box">
-    <div class="container">
-      <div class="box-header">
-        <div class="logo"></div>
-        <h3 class="box-title">LC:RP - Login</h3>
+  <div class="container">
+    <div class="card border-info mx-auto">
+      <div class="container">
+        <div class="card-header">
+          <h3 class="card-title">LC:RP - Login</h3>
+        </div>
+        <form action="post" id="form-login">
+          <div class="card-body">
+            <div class="form-group">
+              <label for="username">Username or Email Address</label>
+              <input type="text" name="username" id="username" class="form-control" placeholder="Nick InGame atau Email" required>
+            </div>
+            <div class="form-group">
+              <label for="pass">Password</label>
+              <input type="password" name="pass" id="pass" class="form-control" placeholder="Password" required>
+            </div>
+          </div>
+          <div class="card-footer">
+            <div class="button-group">
+              <button type="submit" class="btn btn-success" id="submit">Login</button>
+              <a href="./register" class="btn btn-primary">Register</a>
+            </div>
+          </div>
+        </form>
       </div>
-      <form action="post" id="form-login">
-        <div class="box-body">
-          <div class="form-group">
-            <label for="username">Username or Email Address</label>
-            <input type="text" name="username" id="username" class="form-control" placeholder="Nick InGame atau Email" required>
-          </div>
-          <div class="form-group">
-            <label for="pass">Password</label>
-            <input type="password" name="pass" id="pass" class="form-control" placeholder="Password" required>
-          </div>
-        </div>
-        <div class="box-footer">
-          <div class="button-group">
-            <button type="submit" class="btn btn-success" id="submit">Login</button>
-            <a href="./register" class="btn btn-primary">Register</a>
-          </div>
-        </div>
-      </form>
     </div>
   </div>
   <script src="assets/js/jquery/jquery.min.js"></script>
