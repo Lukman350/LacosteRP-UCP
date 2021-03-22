2af315fae5866837af3f394284b13287d4a4f431
Lukman <61648308+Lukman350@users.noreply.github.com>
2021-03-22 12:25:39 +0700

    Update send-user.php

--- vendor/send-user.php
+++ vendor/send-user.php
@@ -7,14 +7,14 @@ include('phpmailer/Exception.php');
 include('phpmailer/PHPMailer.php');
 include('phpmailer/SMTP.php');
 
-$email_pengirim = 'admin@lacosteroleplay.com'; // Isikan dengan email pengirim
+$email_pengirim = 'email@example.com'; // Isikan dengan email pengirim
 $nama_pengirim = 'Lacoste Roleplay'; // Isikan dengan nama pengirim
 $email_penerima = $mailer; // Ambil email penerima dari inputan form
 $nama_penerima = $data["username"];
 $subjek = 'LC:RP Forgot Username'; // Ambil subjek dari inputan form
 $pesan = "
-    <p>To: <br>Email: <b>$mailer</b><br><br>
-        Your account username is: <b>$nama_penerima</b><br><br>
+    <p>To: <br>Email: <b>".$mailer."</b><br><br>
+        Your account username is: <b>".$nama_penerima."</b><br><br>
         Best Regards,<br><br><br><i>LC:RP Management</i><br><br>
         <i style='text-align:center'>Copyright © Lacoste Roleplay 2020.</i>
     </p>
@@ -24,15 +24,15 @@ $pesan = "
 $mail = new PHPMailer;
 $mail->isSMTP();
 
-$mail->Host = 'smtp.hostinger.co.id';
+$mail->Host = 'mail.example.com';
 $mail->Username = $email_pengirim; // Email Pengirim
-$mail->Password = 'Lcrp2307'; // Isikan dengan Password email pengirim
+$mail->Password = 'password123'; // Isikan dengan Password email pengirim
 $mail->Port = 465;
 $mail->SMTPAuth = true;
 $mail->SMTPSecure = 'ssl';
 // $mail->SMTPDebug = 2; // Aktifkan untuk melakukan debugging
 
-$mail->setFrom('admin@lacosteroleplay.com', $nama_pengirim);
+$mail->setFrom($email_pengirim, $nama_pengirim);
 $mail->addAddress($email_penerima, $nama_penerima);
 $mail->isHTML(true); // Aktifkan jika isi emailnya berupa html
 
