91be4a4e8033833f4ad10505e6eba4c0e0f178b3
Lukman350 <nulhakiml725@gmail.com>
2021-03-27 21:44:32 +0700

    Mengganti kirim email dan menggunakan fungsi sendEmail

--- scripts/register.php
+++ scripts/register.php
@@ -1,5 +1,6 @@
 <?php
-include_once "../db/db.php";
+include_once "../config/db.php";
+include_once "../config/email.php";
 $conn = new Database;
 
 $response = [];
@@ -13,7 +14,7 @@ if (isset($_POST["fname"]) && isset($_POST["lname"]) && isset($_POST["email"]) &
     $lname = htmlspecialchars($_POST['lname']);
     $username = $fname."_".$lname;
     $email = htmlspecialchars($_POST['email']);
-    $token = md5(uniqid(rand(), true));
+    $token = base64_encode(random_bytes(32));
     $ip = $conn->getIP();
     $regdate = date("d/m/Y h:i:s");
     $x = 1744.3411;
@@ -42,7 +43,32 @@ if (isset($_POST["fname"]) && isset($_POST["lname"]) && isset($_POST["email"]) &
             $response["status"] = 0;
             $response["message"] = "Password doesn't match!";
         } else {
-            include_once "../vendor/send.php";
+            $pesan = "
+                <p>To: <br>Username: <b>$username</b><br><br>
+                    Please click link in the below to activate your LCRP Account:<br><br>
+                    <a href='https://example.com/register?page=activation&t=".urlencode($token)."'>
+                        https://example.com/register?page=activation&t=".urlencode($token)."
+                    </a><br><br>
+                    Best Regards,<br><br><i>LC:RP Management</i><br><br>
+                    <i style='text-align:center'>Copyright © Lacoste Roleplay 2020.</i>
+                </p>
+            ";
+            if (sendEmail("LC:RP Registration", $email, $username, $pesan)) {
+                if ($gender == 2) {
+                    $skin = 93;
+                } else {
+                    $skin = 2;
+                }
+                $sql = "INSERT INTO `players` (`username`,`ip`,`password`,`salt`,`email`,`token`,`reg_date`,`gender`,`age`,`skin`,`posx`,`posy`,`posz`,`posa`,`brek`,`money`,`bmoney`) VALUES ('$username','$ip','$pass','$salt','$email','$token','$regdate',$gender,'$age',$skin,$x,$y,$z,$a,'$brek',250,200)";
+                $query = $conn->db->prepare($sql);
+                if ($query->execute()) {
+                    $response["status"] = 1;
+                    $response["message"] = 'Your account has been successfuly registered! Please check your email to activate your account.';   
+                }
+            } else {
+                $response["status"] = 0;
+                $response["message"] = 'Something went wrong, email activation not send! Please try again.';
+            }
         }
     }
 }
@@ -54,11 +80,51 @@ if (isset($_POST["e-mail"]) && isset($_POST["pname"])) {
     $query = $conn->db->prepare("SELECT * FROM players WHERE email = '$email'");
     $query->execute();
     if ($query->rowCount() > 0) {
-        include_once "../vendor/send-activate.php";
+        $pesan = "
+            <p>To: <br>Username: <b>$user</b><br><br>
+                Please click link in the below to activate your LCRP Account:<br><br>
+                <a href='https://ucp.lacosteroleplay.com/register?page=activation&t=".urlencode($token)."'>
+                    https://ucp.lacosteroleplay.com/register?page=activation&t=".urlencode($token)."
+                </a><br><br>
+                Best Regards,<br><br><i>LC:RP Management</i><br><br>
+                <i style='text-align:center'>Copyright © Lacoste Roleplay 2020.</i>
+            </p>
+        ";
+        if (sendEmail("LC:RP Account Activation", $email, $user, $pesan)) {
+            $sql = "UPDATE players SET token = '$token', email = '$email' WHERE username = '$user'";
+            $query = $conn->db->prepare($sql);
+            if ($query->execute()) {
+                $response["status"] = 1;
+                $response["message"] = 'Your account has been successfuly registered! Please check your email to activate your account.';
+            }
+        } else {
+            $response["status"] = 0;
+            $response["message"] = 'Something went wrong, email activation not send! Please try again.';
+        }
     } else {
         $sql = $conn->db->prepare("UPDATE players SET email = '$email' WHERE username = '$user'");
         if ($sql->execute()) {
-            include_once "../vendor/send-activate.php";
+            $pesan = "
+                <p>To: <br>Username: <b>$user</b><br><br>
+                    Please click link in the below to activate your LCRP Account:<br><br>
+                    <a href='https://ucp.lacosteroleplay.com/register?page=activation&t=".urlencode($token)."'>
+                        https://ucp.lacosteroleplay.com/register?page=activation&t=".urlencode($token)."
+                    </a><br><br>
+                    Best Regards,<br><br><i>LC:RP Management</i><br><br>
+                    <i style='text-align:center'>Copyright © Lacoste Roleplay 2020.</i>
+                </p>
+            ";
+            if (sendEmail("LC:RP Account Activation", $email, $user, $pesan)) {
+                $sql = "UPDATE players SET token = '$token', email = '$email' WHERE username = '$user'";
+                $query = $conn->db->prepare($sql);
+                if ($query->execute()) {
+                    $response["status"] = 1;
+                    $response["message"] = 'Your account has been successfuly registered! Please check your email to activate your account.';
+                }
+            } else {
+                $response["status"] = 0;
+                $response["message"] = 'Something went wrong, email activation not send! Please try again.';
+            }
         }
     }
 }
